fc4de3591c9cd37fd22d847974e6694bed443374
general-soldatov <general-soldatov@mail.ru>
2025-07-05 10:28:53 +0300

    Create php

---
+++ display.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Первый сайт на PHP</title>
+<meta charset="utf-8">
+</head>
+<body>
+<?php
+$name = $_POST["firstname"];
+$surname = $_POST["lastname"];
+echo "Ваше имя: <b>".$name . " " . $surname . "</b>";
+?>
+</body>
+</html>
